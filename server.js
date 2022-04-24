6d2f767136a3475dd8866d47b0eaa04cc1ba6935
Martin <nitromagix@gmail.com>
2022-04-24 10:55:40 -0700

    fixed bug

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-import express from 'express'
+const express = require('express')
 const app = express()
-import {
-   start
-} from 'live-server'
+const liveServer = require('live-server')
 
 async function main() {
-   app.listen(3001, () => {
-      start({
-         port: 3000,
-         logLevel: 0,
-         root: './public'
-      })
-   })
+
+    app.listen(3001, () => {
+        liveServer.start({
+            port: 3000,
+            logLevel: 0,
+            root: './public'
+        })
+    })
 }
 
 main()
